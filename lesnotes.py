ff94ddf05864b8787b4f17bd456b6c571456b1cc
Leonardo Esteban Stazi <estebanstazi@gmail.com>
2023-05-01 20:16:00 -0300

    Mejora a la función save_file, agregamos la extensión Python, cambios a los botones.

--- lesnotes.py
+++ lesnotes.py
@@ -3,16 +3,26 @@ from tkinter.filedialog import *
 from tkinter import LabelFrame
 
 
+current_file = None
+
 def save_file():
-    new_file = asksaveasfile(mode="w", filetypes=[("text files", ".txt")])
-    if new_file is None:
-        return
+    global current_file
+    if current_file is None:
+        new_file = asksaveasfile(mode="w", filetypes=[("All files", "*.*"), 
+                                                      ("Text files", "*.txt"), 
+                                                      ("Python files", "*.py")])
+        if new_file is None:
+            return
+        current_file = new_file.name
+
     text = str(body.get("1.0", tk.END))
-    new_file.write(text)
-    new_file.close()
+    with open(current_file, 'w') as f:
+        f.write(text)
 
 def open_file():
-    file = askopenfile(mode="r", filetypes=[("text files", "*.txt")])
+    file = askopenfile(mode="r", filetypes=[("All files", "*.*"), 
+                                            ("Text files", "*.txt"),
+                                            ("Python files", "*.py")])
     if file is not None:
         file.seek(0)
         content = file.read()
@@ -24,7 +34,7 @@ def clear_window():
 
 
 lienzo = tk.Tk()
-lienzo.geometry("400x400")
+lienzo.geometry("800x600")
 lienzo.title("LesNotes")
 lienzo.config(bg="black")
 
@@ -34,13 +44,17 @@ head.pack(fill= "x", side="top")
 border = LabelFrame(lienzo, bd = 6, bg = "black")
 border.pack(pady = 10)
 
-b1 = tk.Button(border, text="Open", width=12, bg="#6CD300", fg = "black", command=open_file)
+b1 = tk.Button(border, text="Open", width=15, bg="#6CD300", fg = "black", 
+               font=("Cascadia Code", 10, "bold"), command=open_file)
 b1.pack(side="left")
-b2 = tk.Button(border, text="Save", width=12, bg="#6CD300", fg = "black", command=save_file)
+b2 = tk.Button(border, text="Save", width=15, bg="#6CD300", fg = "black", 
+               font=("Cascadia Code", 10, "bold"), command=save_file)
 b2.pack(side="left")
-b3 = tk.Button(border, text="Clear", width=12, bg="#6CD300", fg = "black", command=clear_window)
+b3 = tk.Button(border, text="Clear", width=15, bg="#6CD300", fg = "black", 
+               font=("Cascadia Code", 10, "bold"), command=clear_window)
 b3.pack(side="left")
-b4 = tk.Button(border, text="Exit", width=12, bg="#6CD300", fg = "black", command=exit)
+b4 = tk.Button(border, text="Exit", width=15, bg="#6CD300", fg = "black", 
+               font=("Cascadia Code", 10, "bold"), command=exit)
 b4.pack(side="left")
 
 body = tk.Text(lienzo, wrap="word", bg="black", insertbackground="#6CD300", 
