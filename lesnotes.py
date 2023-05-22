9199905cecb9273253172e8c6b229119d268a09f
Leonardo Esteban Stazi <estebanstazi@gmail.com>
2023-05-21 21:59:51 -0300

    LesNotes v1.1

--- lesnotes.py
+++ lesnotes.py
@@ -5,8 +5,40 @@ from tkinter import LabelFrame, messagebox
 
 current_file = None
 
-def save_file():
+def open_file(event=None):
     global current_file
+
+    file = askopenfile(mode="r", filetypes=[("All files", "*.*"), 
+                                            ("Text files", "*.txt"),
+                                            ("Python files", "*.py")])
+    if file is not None:
+        file.seek(0)
+        content = file.read()
+        body.delete("1.0", tk.END)
+        body.insert(tk.INSERT, content)
+        current_file = file.name
+ 
+def close_file(event=None):
+    global current_file
+
+    if current_file:
+        response = messagebox.askyesnocancel("Close", "Save changes before closing?")
+
+        if response:
+            save_file()
+            body.delete("1.0", tk.END)
+            current_file = None
+
+        elif response is False:
+            body.delete("1.0", tk.END)
+            current_file = None
+
+        else:
+            return
+
+def save_file(event=None):
+    global current_file
+
     if current_file is None:
         new_file = asksaveasfile(mode="w", filetypes=[("All files", "*.*"), 
                                                       ("Text files", "*.txt"), 
@@ -22,26 +54,27 @@ def save_file():
     new_file.write(text)
     new_file.close()
 
-def open_file():
-    global current_file
-    file = askopenfile(mode="r", filetypes=[("All files", "*.*"), 
-                                            ("Text files", "*.txt"),
-                                            ("Python files", "*.py")])
-    if file is not None:
-        file.seek(0)
-        content = file.read()
-        body.delete("1.0", tk.END)
-        body.insert(tk.INSERT, content)
-        current_file = file.name
- 
-def clear_window():
+def clear_window(event=None):
     body.delete("1.0", tk.END)
 
-def exit_app():
-    if body.edit_modified():
-        if messagebox.askyesno("Exit", "Save changes?"):
+def exit_app(event=None):
+    global current_file
+
+    if current_file:
+        response = messagebox.askyesnocancel("Exit", "Save changes?")
+
+        if response:
             save_file()
-    lienzo.destroy()
+            lienzo.destroy()
+
+        elif response is False:
+            lienzo.destroy()
+        
+        else:
+            return
+
+    else:
+        lienzo.destroy()
 
 
 lienzo = tk.Tk()
@@ -58,18 +91,27 @@ border.pack(pady=10)
 b1 = tk.Button(border, text="Open", width=15, bg="#6CD300", fg="black", 
                font=("Cascadia Code", 10, "bold"), command=open_file)
 b1.pack(side="left")
-b2 = tk.Button(border, text="Save", width=15, bg="#6CD300", fg="black", 
-               font=("Cascadia Code", 10, "bold"), command=save_file)
+b2 = tk.Button(border, text="Close", width=15, bg="#6CD300", fg="black", 
+               font=("Cascadia Code", 10, "bold"), command=close_file)
 b2.pack(side="left")
-b3 = tk.Button(border, text="Clear", width=15, bg="#6CD300", fg="black", 
-               font=("Cascadia Code", 10, "bold"), command=clear_window)
+b3 = tk.Button(border, text="Save", width=15, bg="#6CD300", fg="black", 
+               font=("Cascadia Code", 10, "bold"), command=save_file)
 b3.pack(side="left")
-b4 = tk.Button(border, text="Exit", width=15, bg="#6CD300", fg="black", 
-               font=("Cascadia Code", 10, "bold"), command=exit_app)
+b4 = tk.Button(border, text="Clear", width=15, bg="#6CD300", fg="black", 
+               font=("Cascadia Code", 10, "bold"), command=clear_window)
 b4.pack(side="left")
+b5 = tk.Button(border, text="Exit", width=15, bg="#6CD300", fg="black", 
+               font=("Cascadia Code", 10, "bold"), command=exit_app)
+b5.pack(side="left")
 
 body = tk.Text(lienzo, wrap="word", bg="black", insertbackground="#6CD300", 
                fg="#6CD300", font=("Cascadia Code", 12))
 body.pack(padx=10, pady=5, expand=True, fill="both")
+
+lienzo.bind("<Control-o>", open_file)
+lienzo.bind("<Control-c>", close_file)
+lienzo.bind("<Control-s>", save_file) 
+lienzo.bind("<Control-d>", clear_window)
+lienzo.bind("<Control-e>", exit_app)
 
 lienzo.mainloop( )
